f93dac7db8ca7ef02facbdf54d0a1a2139958fce
U-Honolulu\Troy <me@bitcointroy.co>
2018-03-02 14:32:38 -0600

    Essays

--- Answers.md
+++ Answers.md
@@ -1,1 +1,12 @@
 <!-- Answers to the Short Answer Essay Questions go here -->
+
+## Authentication Sprint Challenge
+
+### Troy Bradley - CS6
+
+1. a. Middleware: functions that help pass information between the client and server, while also doing some processing of that information before it is presented at it's destination. It also helps to maintain the correct order of processing steps by telling the program where to go "next", once it has resolved a Promise.
+   b. Sessions: Sessions in express.js allow programmers to set attributes on a specific user of an app, and limit or expand their access to resources based on those attributes. isLoggedIn may allow access to their profile within the app, for example.
+   c. bcrypt: is a module that works with node.js to provide hash and compare functions that allow programmers to safely store and use passwords to authenticate users.  
+   d. JWT (jots): JSON Web Tokens are an alternative to sessions used to provide the same functionality.
+1. bcrypt vs. attackers: bcrypt includes a concept called "SaltRounds", which interjects a delay in the process of hashing a password. This delay effectively thwarts dictionary attacks (using known words to "guess" a password) by making the process take too long. There are millions of possible entries in a "dictionary" used in such an attack, so every millisecond that it takes to hash a password guess and return a response from the server means thousands of seconds to complete a dictionary attack. There are "only" 86,400 seconds in 24 hours.
+1. A JSON Web Token includes a header, payload, and signature.
--- api/utils/middlewares.js
+++ api/utils/middlewares.js
@@ -6,39 +6,40 @@ const { mysecret } = require('../../config');
 const SaltRounds = 11;
 
 const authenticate = (req, res, next) => {
-  const token = req.get('Authorization');
-  if (token) {
-    jwt.verify(token, mysecret, (err, decoded) => {
-      if (err) return res.status(422).json(err);
-      req.decoded = decoded;
-      next();
-    });
-  } else {
-    return res.status(403).json({
-      error: 'No token provided, must be set on the Authorization Header'
-    });
-  }
+	const token = req.get('Authorization');
+	if (token) {
+		jwt.verify(token, mysecret, (err, decoded) => {
+			if (err) return res.status(422).json(err);
+			req.decoded = decoded;
+			next();
+		});
+	} else {
+		return res.status(403).json({
+			error: 'No token provided, must be set on the Authorization Header'
+		});
+	}
 };
 
 const encryptUserPW = (req, res, next) => {
-  const { username, password } = req.body;
-  // https://github.com/kelektiv/node.bcrypt.js#usage
-  // TODO: Fill this middleware in with the Proper password encrypting, bcrypt.hash()
-  // Once the password is encrypted using bcrypt you'll need to set a user obj on req.user with the encrypted PW
-  // Once the user is set, call next and head back into the userController to save it to the DB
+	const { username, password } = req.body;
+	// https://github.com/kelektiv/node.bcrypt.js#usage
+	// TODO: Fill this middleware in with the Proper password encrypting, bcrypt.hash()
+	// Once the password is encrypted using bcrypt you'll need to set a user obj on req.user with the encrypted PW
+	// Once the user is set, call next and head back into the userController to save it to the DB
+	req.body.passwordHash = bcrypt.hash(password, SaltRounds);
 };
 
 const compareUserPW = (req, res, next) => {
-  const { username, password } = req.body;
-  // https://github.com/kelektiv/node.bcrypt.js#usage
-  // TODO: Fill this middleware in with the Proper password comparing, bcrypt.compare()
-  // You'll need to find the user in your DB
-  // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
-  // If the passwords match set the username on `req` ==> req.username = user.username; and call next();
+	const { username, password } = req.body;
+	// https://github.com/kelektiv/node.bcrypt.js#usage
+	// TODO: Fill this middleware in with the Proper password comparing, bcrypt.compare()
+	// You'll need to find the user in your DB
+	// Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+	// If the passwords match set the username on `req` ==> req.username = user.username; and call next();
 };
 
 module.exports = {
-  authenticate,
-  encryptUserPW,
-  compareUserPW
+	authenticate,
+	encryptUserPW,
+	compareUserPW
 };
