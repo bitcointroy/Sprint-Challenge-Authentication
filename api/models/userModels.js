ca06d2223ef9b4f5643e01ee5a695f6991d7afed
U-Honolulu\Troy <me@bitcointroy.co>
2018-03-02 11:38:34 -0600

    UserSchema

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = Schema({
-  // create your user schema here.
+	username: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	password: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
